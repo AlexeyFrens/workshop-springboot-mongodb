5ba214da437f2ac24da095c562fcb594e0cc95ca
João Cláudio <leiloka46@gmail.com>
2024-12-06 19:09:29 -0300

    Projeto criado

---
+++ src/test/java/com/alexeyfrens/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexeyfrens.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
