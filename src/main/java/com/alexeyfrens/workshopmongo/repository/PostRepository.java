11381922f3ea41ed0129244ff1183f306219d949
João Cláudio <leiloka46@gmail.com>
2024-12-11 18:33:42 -0300

    Criando entidade Post com User aninhado

---
+++ src/main/java/com/alexeyfrens/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.alexeyfrens.workshopmongo.repository;
+
+import com.alexeyfrens.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
